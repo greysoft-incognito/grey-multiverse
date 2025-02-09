344708ff6cb5dde57f2072149b7f975aa894b898
3m1n3nc3 <mygames.ng@gmail.com>
2025-02-09 14:14:35 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+echo "Pulling latest changes from Git..."
 git pull origin main
 
-composer install --no-dev --optimize-autoloader
+echo "Installing dependencies..."
+COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --optimize-autoloader -n
 
+echo "Running migrations..."
 php artisan migrate --force
-php artisan cache:clear
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-php artisan view:cache
+
+echo "Refreshing cache..."
+php artisan optimize:clear
+php artisan optimize
+
+echo "Syncronizing Roles..."
 php artisan app:sync-roles
+
+echo "Setting permissions..."
+chown -R greysoft-multiverse:greysoft-multiverse $(pwd)
+chmod -R 775 storage bootstrap/cache
+
+echo "Deployment complete."
