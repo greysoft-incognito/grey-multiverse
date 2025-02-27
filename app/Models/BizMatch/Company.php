12b4ccea43397f6871ef1d49ad4a532fe51abd3f
3m1n3nc3 <mygames.ng@gmail.com>
2025-02-27 08:45:35 +0100

    fix: Fix Company model search scope.

--- app/Models/BizMatch/Company.php
+++ app/Models/BizMatch/Company.php
@@ -4,6 +4,7 @@ namespace App\Models\BizMatch;
 
 use App\Models\User;
 use App\Traits\ModelCanExtend;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
