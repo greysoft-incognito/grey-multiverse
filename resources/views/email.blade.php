875f747b8165ff8a12c66f589334039970c4c355
3m1n3nc3 <mygames.ng@gmail.com>
2025-03-02 10:19:34 +0100

    feat: Add config to Disable Email Banners

--- resources/views/email.blade.php
+++ resources/views/email.blade.php
@@ -83,67 +83,72 @@
         style="display:none;font-size:1px;color:#ffffff;line-height:1px;max-height:0px;max-width:0px;opacity:0;overflow:hidden;">
         {!! str(collect($lines)->filter(fn($l) => is_string($l))->join(' '))->limit(100) ?: $subject !!} </div>
     <div style="background-color:#ffffff;">
-        <div style="margin:0px auto;max-width:600px;">
-            <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation"
-                style="width:100%;">
-                <tbody>
-                    <tr>
-                        <td style="direction:ltr;font-size:0px;padding:20px 0;padding-bottom:0px;text-align:center;">
-                            <div class="mj-column-per-100 mj-outlook-group-fix"
-                                style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
-                                <table border="0" cellpadding="0" cellspacing="0" role="presentation"
-                                    style="vertical-align:top;" width="100%">
-                                    <tbody>
-                                        <tr>
-                                            <td align="left"
-                                                style="font-size:0px;padding:10px 25px;word-break:break-word;">
-                                                <table border="0" cellpadding="0" cellspacing="0"
-                                                    role="presentation"
-                                                    style="border-collapse:collapse;border-spacing:0px;">
-                                                    <tbody>
-                                                        <tr>
-                                                            <td style="width:100px;"> <img alt="Logo" height="auto"
-                                                                    src="{{ $logo ?? dbconfig('app_logo') }}?v=2"
-                                                                    style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;"
-                                                                    width="100" /> </td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td style="font-size:0px;word-break:break-word;">
-                                                <div style="height:20px;">   </div>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td align="center" style="font-size:0px;padding:0;word-break:break-word;">
-                                                <table border="0" cellpadding="0" cellspacing="0"
-                                                    role="presentation"
-                                                    style="border-collapse:collapse;border-spacing:0px;">
-                                                    <tbody>
-                                                        <tr>
-                                                            <td style="width:600px;">
-                                                                <a href="{{ dbconfig('app_url') }}" target="_blank"
-                                                                    style="color: #2e58ff; text-decoration: none;"> <img
-                                                                        alt="image description" height="200"
-                                                                        src="{{ $banner ?? dbconfig('email_banner') }}?v=2"
-                                                                        style="border:0;display:block;outline:none;text-decoration:none;height:200px;width:100%;font-size:13px;object-fit:cover;"
-                                                                        width="auto" /> </a>
-                                                            </td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        @if (!dbconfig('disable_email_banners'))
+            <div style="margin:0px auto;max-width:600px;">
+                <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation"
+                    style="width:100%;">
+                    <tbody>
+                        <tr>
+                            <td
+                                style="direction:ltr;font-size:0px;padding:20px 0;padding-bottom:0px;text-align:center;">
+                                <div class="mj-column-per-100 mj-outlook-group-fix"
+                                    style="font-size:0px;text-align:left;direction:ltr;display:inline-block;vertical-align:top;width:100%;">
+                                    <table border="0" cellpadding="0" cellspacing="0" role="presentation"
+                                        style="vertical-align:top;" width="100%">
+                                        <tbody>
+                                            <tr>
+                                                <td align="left"
+                                                    style="font-size:0px;padding:10px 25px;word-break:break-word;">
+                                                    <table border="0" cellpadding="0" cellspacing="0"
+                                                        role="presentation"
+                                                        style="border-collapse:collapse;border-spacing:0px;">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td style="width:100px;"> <img alt="Logo"
+                                                                        height="auto"
+                                                                        src="{{ $logo ?? dbconfig('app_logo') }}?v=2"
+                                                                        style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;"
+                                                                        width="100" /> </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="font-size:0px;word-break:break-word;">
+                                                    <div style="height:20px;">   </div>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="center"
+                                                    style="font-size:0px;padding:0;word-break:break-word;">
+                                                    <table border="0" cellpadding="0" cellspacing="0"
+                                                        role="presentation"
+                                                        style="border-collapse:collapse;border-spacing:0px;">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td style="width:600px;">
+                                                                    <a href="{{ dbconfig('app_url') }}" target="_blank"
+                                                                        style="color: #2e58ff; text-decoration: none;">
+                                                                        <img alt="image description" height="200"
+                                                                            src="{{ $banner ?? dbconfig('email_banner') }}?v=2"
+                                                                            style="border:0;display:block;outline:none;text-decoration:none;height:200px;width:100%;font-size:13px;object-fit:cover;"
+                                                                            width="auto" /> </a>
+                                                                </td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        @endif
         <div style="margin:0px auto;max-width:600px;">
             <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation"
                 style="width:100%;">
