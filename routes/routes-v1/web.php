bf5c2012564dd741ce635535be179d8f89dbf194
3m1n3nc3 <mygames.ng@gmail.com>
2025-03-06 15:21:27 +0100

    fix: Fix wrong dependencies for qr code.

--- routes/routes-v1/web.php
+++ routes/routes-v1/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Models\GenericFormData;
+use App\Models\FormData;
 use App\Models\Reservation;
 use App\Services\HttpStatus;
 use Illuminate\Support\Facades\Response;
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{type}/data/qr/{id}', function ($type, $id) {
     // header('Content-Type: image/png');
     if ($type === 'form') {
-        $data = GenericFormData::findOrFail($id);
+        $data = FormData::findOrFail($id);
         $encoded = "grey:multiverse:form={$data->form_id}:data={$data->id}";
     } elseif ($type === 'reservation') {
         $data = Reservation::findOrFail($id);
